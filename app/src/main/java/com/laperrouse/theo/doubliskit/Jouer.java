eb594c30b62d6197d7a23bf7058cd84fe170b07e
Théo Laperrouse <theo.laperrouse@laposte.net>
2019-02-11 04:13:45 +0100

    Version Finale
    Apk

--- app/src/main/java/com/laperrouse/theo/doubliskit/Jouer.java
+++ app/src/main/java/com/laperrouse/theo/doubliskit/Jouer.java
@@ -3,8 +3,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
-import android.view.animation.AnimationUtils;
-import android.view.animation.RotateAnimation;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
